ef0822cfe69b364199023bf536be0f1c12788ae9
twilson <twilson@rbc.org>
2014-09-18 09:47:26 -0400

    lightbox fixes

--- wp-content/themes/my-portfolio/js/theme.js
+++ wp-content/themes/my-portfolio/js/theme.js
@@ -8,13 +8,13 @@ jQuery("#logo").hide().fadeIn(1000);
    Lightbox
    ========================================================================== */
 var $overlay = jQuery('<div id="overlay"></div>');
-var $image = jQuery('<img>');
+var $image = jQuery('<div class="container"><div class="col-md-10 col-md-offset-1"><img></div></div>');
 //open lightbox when image is clicked
 jQuery(".project-images a").click(function(event) {
     event.preventDefault();
     $overlay.append($image);
     jQuery("body").append($overlay);
-    $image.attr("src", jQuery(this).attr("href"));
+    jQuery("#overlay img").attr("src", jQuery(this).attr("href"));
     $overlay.fadeIn("fast");
 });
 
