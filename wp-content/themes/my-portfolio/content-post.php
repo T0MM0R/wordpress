69d895f772e0a57f0174e65911592afd57e44fa4
twilson <twilson@rbc.org>
2014-09-23 10:16:34 -0400

    fix display property for author information

--- wp-content/themes/my-portfolio/content-post.php
+++ wp-content/themes/my-portfolio/content-post.php
@@ -3,9 +3,9 @@
         <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
         <ul class="info">
             <?php if(is_single()): ?><li class="img album"><?php echo get_custom_avatar( get_the_author_meta('user_email') , 100 , 'mysteryman' , 'gravatar' ); ?></li><?php endif; ?>
-            <li>Author: <a href="<?php bloginfo('siteurl') ;?>/about/"><?php the_author(); ?></a></li>
+            <li><a href="<?php bloginfo('siteurl') ;?>/about/"><?php the_author(); ?></a></li>
             <li>Posted in: <?php the_category(', '); ?></li>
-            <li>Date: <?php the_time('F j, Y'); ?></li>
+            <li><?php the_time('F j, Y'); ?></li>
         </ul> 
     </div>
 
