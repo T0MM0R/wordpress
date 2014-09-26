efc59de2439cf010e7c3e48497e354e419736157
twilson <twilson@rbc.org>
2014-09-26 11:26:37 -0400

    change layout of contact page

--- wp-content/themes/my-portfolio/contact.php
+++ wp-content/themes/my-portfolio/contact.php
@@ -10,9 +10,9 @@ get_header();
 <div class="container">
     
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <h5><?php the_title(); ?></h5>
     
     <div class="about album col-md-8">
-        <h5><?php the_title(); ?></h5>
         <?php the_content(); ?>
     </div>
     
