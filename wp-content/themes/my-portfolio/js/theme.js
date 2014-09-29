92391ed7f89b5ea42d59cd92653fc94afbda7127
twilson <twilson@rbc.org>
2014-09-29 14:37:58 -0400

    add bs-image-gallery

--- wp-content/themes/my-portfolio/js/theme.js
+++ wp-content/themes/my-portfolio/js/theme.js
@@ -7,8 +7,15 @@ jQuery("#logo").hide().fadeIn(1000);
 blueimp.Gallery(
     document.getElementById('gallery').getElementsByTagName('a'),
     {
-        container: '#blueimp-gallery-carousel',
-        carousel: true
+        hidePageScrollbars: false,
+        toggleControlsOnReturn: false,
+        toggleSlideshowOnSpace: true,
+        enableKeyboardNavigation: true,
+        closeOnEscape: false,
+        closeOnSlideClick: false,
+        closeOnSwipeUpOrDown: false,
+        disableScroll: false,
+        startSlideshow: true
     }
 );
 
