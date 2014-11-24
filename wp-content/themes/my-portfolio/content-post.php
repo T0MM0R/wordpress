b8301eb0261abe3a51cfc4eafc38b640773971d0
twilson <twilson@rbc.org>
2014-11-24 09:26:07 -0500

    updated layout for content-posts

--- wp-content/themes/my-portfolio/content-post.php
+++ wp-content/themes/my-portfolio/content-post.php
@@ -1,39 +1,49 @@
-<div <?php if (has_post_thumbnail()) { echo "class=\"col-md-6\""; } ?>>
-    <?php if(is_single()): ?>
-        <div class="album pull-right"><?php echo get_custom_avatar( get_the_author_meta('user_email') , 100 , 'mysteryman' , 'gravatar' ); ?></div>
-    <?php endif; ?>
-    <div class="title">
-        <h3><?php if(!is_single()) :?><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><?php endif; ?></h3>
-        <ul class="info">
-            <li><?php the_author(); ?></li>
-            <li>Posted in: <?php the_category(', '); ?></li>
-            <li><?php the_time('F j, Y'); ?></li>
-        </ul>
+    <div class="<?php echo (has_post_thumbnail()) ? "col-md-6" : ""; ?>">
+    
+        <div class="title">
+            <h3>
+                <?php if(!is_single()) :?><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><?php else: ?>
+                <?php the_title(); ?>
+                <?php endif; ?>
+            </h3>
+            <ul class="info">
+                <li>By: <?php the_author(); ?></li>
+                <li>Published in <?php the_category(', '); ?></li>
+                <li>On <?php the_time('F j, Y'); ?></li>
+            </ul>
+        </div>
         
-    </div>
-        
-    <?php if(is_single()): ?>
+        <?php if(is_single()): ?>
 
-        <article>
+            <article>
 
-            <?php the_content(); ?>
+                <?php the_content(); ?>
+                <div class="album pull-left">
+                    <?php echo get_custom_avatar( get_the_author_meta('user_email'), 100 , 'mysteryman' , 'gravatar' ); ?>
+                </div>
+                <div class="author col-md-8">
+                    <h1><?php the_author(); ?></h1>
+                    <ul class="info">
+                        <li><a href="mailto:<?php the_author_meta('user_email'); ?>"><i class="fa fa-envelope"></i></a></li>
+                        <li><a href="<?php the_author_meta('user_url'); ?>" target="_blank"><i class="fa fa-home"></i></a></li>
+                        <li><a href="<?php the_author_meta('twitter'); ?>"><i class="fa fa-twitter fa-2"></i></a></li>
+                        <li><a href="<?php the_author_meta('facebook'); ?>"><i class="fa fa-facebook"></i></a></li>
+                    </ul>
+                    <p><?php the_author_meta('description'); ?></p>
+                </div>
 
-        </article>
+            </article>
 
     <?php else: ?>
-        
-    <div class="excerpt">
 
-        <p>
-            <?php the_excerpt(); ?>
-            <a class="post-link" href="<?php the_permalink(); ?>">Continue Reading &rarr;</a>
-        </p>
-
-    </div>
+            <p>
+                <?php the_excerpt(); ?>
+                <a class="post-link" href="<?php the_permalink(); ?>">Continue Reading &rarr;</a>
+            </p>
         
     <?php endif; ?>
-    
-</div>
+            
+    </div>
 
 <?php if (has_post_thumbnail()) :?>
     <?php if (is_front_page() && in_category("featured")) : ?>
