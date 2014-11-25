e3e9d4b683f7eb6b8ddb214d0819b6e520cfb5bb
twilson <twilson@rbc.org>
2014-11-25 11:30:02 -0500

    modified content-author template

--- wp-content/themes/my-portfolio/content-author.php
+++ wp-content/themes/my-portfolio/content-author.php
@@ -17,15 +17,15 @@
     <?php foreach ( $user_query->results as $user ) : ?>
         <div class="row">
             <div class="album pull-left">
-                <?php echo get_custom_avatar( $user->user_email , 100 , 'mysteryman' , 'gravatar' ); ?>
+                <?php echo get_custom_avatar( the_author_meta('user_email') , 100 , 'mysteryman' , 'gravatar' ); ?>
             </div>
             <div class="col-md-8">
                 <h1><?php echo $user->display_name; ?></h1>
                 <ul class="info">
-                    <li><a href="mailto:<?php echo $user->user_email; ?>"><i class="fa fa-envelope"></i></a></li>
-                    <li><a href="<?php echo $user->user_url; ?>" target="_blank"><i class="fa fa-home"></i></a></li>
-                    <li><a href="<?php echo get_the_author_meta('twitter', $user->ID); ?>"><i class="fa fa-twitter fa-2"></i></a></li>
-                    <li><a href="<?php echo get_the_author_meta('facebook', $user->ID); ?>"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="mailto:<?php the_author_meta('user_email', $user->ID); ?>"><i class="fa fa-envelope"></i></a></li>
+                    <li><a href="<?php the_author_meta('user_url', $user->ID); ?>" target="_blank"><i class="fa fa-home"></i></a></li>
+                    <li><a href="<?php the_author_meta('twitter', $user->ID); ?>"><i class="fa fa-twitter fa-2"></i></a></li>
+                    <li><a href="<?php the_author_meta('facebook', $user->ID); ?>"><i class="fa fa-facebook"></i></a></li>
                 </ul>
                 <div class="clearfix"></div>
                 <?php if (is_page('contact')) : ?>
