b7342df8bd59a4b450891ec5e21744b2b21349e5
twilson <twilson@rbc.org>
2014-11-26 17:19:09 -0500

    load blue imp gallery on document ready

--- wp-content/themes/my-portfolio/js/theme.js
+++ wp-content/themes/my-portfolio/js/theme.js
@@ -18,6 +18,24 @@ jQuery(document).ready(function() {
         
     });
     
+    blueimp.Gallery(
+    document.getElementById('gallery').getElementsByTagName('a'),
+    {
+        container: '#blueimp-gallery-carousel',
+        carousel: true,
+        hidePageScrollbars: false,
+        toggleControlsOnReturn: false,
+        toggleSlideshowOnSpace: false,
+        enableKeyboardNavigation: true,
+        closeOnEscape: false,
+        closeOnSlideClick: false,
+        closeOnSwipeUpOrDown: false,
+        disableScroll: false,
+        startSlideshow: true
+    }
+);
+
+    
 });
 
 jQuery(document).scroll(function(){
@@ -35,22 +53,6 @@ jQuery(document).scroll(function(){
 
 jQuery("#logo").hide().fadeIn(1000);
 
-blueimp.Gallery(
-    document.getElementById('gallery').getElementsByTagName('a'),
-    {
-        container: '#blueimp-gallery-carousel',
-        carousel: true,
-        hidePageScrollbars: false,
-        toggleControlsOnReturn: false,
-        toggleSlideshowOnSpace: false,
-        enableKeyboardNavigation: true,
-        closeOnEscape: false,
-        closeOnSlideClick: false,
-        closeOnSwipeUpOrDown: false,
-        disableScroll: false,
-        startSlideshow: true
-    }
-);
 
 
 
