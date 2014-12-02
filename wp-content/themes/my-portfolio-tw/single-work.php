fc748af908daf4145bbc6812feaac8d2f46dd09f
twilson <twilson@rbc.org>
2014-12-02 14:43:59 -0500

    updated single-work

--- wp-content/themes/my-portfolio-tw/single-work.php
+++ wp-content/themes/my-portfolio-tw/single-work.php
@@ -1,31 +1,38 @@
 <?php get_header();?>
 
 <div class="row">
-    <div class="container card">
+    
+    <div class="container">
 
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <h3><?php the_title(); ?></h3>
         
-        <hr>
+        <div class="card">
+        
+            <h3><?php the_title(); ?></h3>
 
-        <div class="project-images col-md-12 no-padding">
+            <hr>
+
+            <div class="project-images col-md-12 no-padding">
+
+                <article>
+
+                    <?php the_content(); ?>
+
+                </article>
+
+
+            </div>
+
+            <?php endwhile; else: ?>
+
+                <p>Page not found</p>
+
+            <?php endif; ?>
             
-            <article>
-
-                <?php the_content(); ?>
-                
-            </article>
-            
-
         </div>
 
-        <?php endwhile; else: ?>
-
-            <p>Page not found</p>
-
-        <?php endif; ?>
-
     </div>
+    
 </div>
 
 <?php get_footer();?>
