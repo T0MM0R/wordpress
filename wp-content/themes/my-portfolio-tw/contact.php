e2fa964bd3c8e8d227222135d805ef9532049dcd
twilson <twilson@rbc.org>
2014-12-05 09:54:46 -0500

    modified contact template in child theme

---
+++ wp-content/themes/my-portfolio-tw/contact.php
@@ -0,0 +1,34 @@
+<?php
+
+/* Template Name: Contact Page
+ * 
+ */
+
+get_header();
+
+?>
+<div class="container">
+    
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <h5><?php the_title(); ?></h5>
+    
+    <div class="about album col-md-8">
+        <?php the_content(); ?>
+    </div>
+    
+    <?php endwhile; endif; ?>
+    
+    <div class="about album col-md-4">
+        <?php if ( has_post_thumbnail() ) : ?>
+            <?php the_post_thumbnail( 'large', array( 'class' => 'img-responsive margin-bottom-10 visible-md visible-lg' ) ) ?>
+        <?php endif; ?>
+    </div>
+    
+    <div class="row">
+        <div class="col-md-12">
+            <?php get_template_part('content', 'author'); ?>
+        </div>
+    </div>
+</div>
+
+<?php get_footer();?>
