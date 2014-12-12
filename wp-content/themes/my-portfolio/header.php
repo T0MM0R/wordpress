022a760957fa0b3100e7d11030a75bcda665f50c
twilson <twilson@rbc.org>
2014-12-12 13:52:08 -0500

    trouble shooting horizontal scroll

--- wp-content/themes/my-portfolio/header.php
+++ wp-content/themes/my-portfolio/header.php
@@ -17,34 +17,30 @@
     
     <body <?php if (is_front_page()) { echo "class='home'"; } ?>>
         
-        <div id="nav" class="navbar navbar-default navbar-fixed-top">
-            <header>
-                <div class="container-fluid">
-                    <hgroup id="logo" class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav1">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        
-                        <a href="<?php echo site_url(); ?>">
-                            <h1><?php bloginfo( 'name' ); ?></h1>
-                            <h2><?php bloginfo( 'description' ) ;?></h2>
-                        </a>
-                        
-                    </hgroup>
-                    <nav class="navbar-collapse collapse" id="nav1">
-                       <?php
-                            $args = array(
-                                'menu', 
-                                'main',
-                                'menu_class'      => 'nav navbar-nav navbar-right'
-                            );
-                            wp_nav_menu( $args );
-                        ?>
-                    </nav>
-                </div>
-            </header>
-        </div>
+        <header id="nav" class="navbar navbar-default navbar-fixed-top container-fluid">
+            <hgroup id="logo" class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <a href="<?php echo site_url(); ?>">
+                    <h1><?php bloginfo( 'name' ); ?></h1>
+                    <h2><?php bloginfo( 'description' ) ;?></h2>
+                </a>
+
+            </hgroup>
+            <nav class="navbar-collapse collapse" id="nav1">
+               <?php
+                    $args = array(
+                        'menu', 
+                        'main',
+                        'menu_class'      => 'nav navbar-nav navbar-right'
+                    );
+                    wp_nav_menu( $args );
+                ?>
+            </nav>
+        </header>
         
