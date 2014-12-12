263c9b7aba56e0ba68ab0f2fd9d53109ba05adbd
twilson <twilson@rbc.org>
2014-12-12 15:47:05 -0500

    trouble shooting horizontal scroll

--- wp-content/themes/my-portfolio/front-page.php
+++ wp-content/themes/my-portfolio/front-page.php
@@ -31,31 +31,26 @@
 
     <div id="content" class="container">
     
+        <h5>Featured Post</h5>
+        <article>
+            <?php
+            wp_reset_postdata();
+            $args = array(
+                'post_type' => 'post',
+                'category_name' => 'featured',
+                'posts_per_page' => 1
+            );
 
-        <div class="row">
-            <div class="col-md-12">
-                <h5>Featured Post</h5>
-            </div>
-            <article>
-                <?php
-                wp_reset_postdata();
-                $args = array(
-                    'post_type' => 'post',
-                    'category_name' => 'featured',
-                    'posts_per_page' => 1
-                );
+            $the_query = new WP_Query( $args );
 
-                $the_query = new WP_Query( $args );
+            ?>
+            <?php if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
-                ?>
-                <?php if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-                
 
-                    <?php get_template_part( 'content', 'post' ); ?>
+                <?php get_template_part( 'content', 'post' ); ?>
 
-            </article>
-            <?php endwhile; endif; ?>
-        </div>
+        </article>
+        <?php endwhile; endif; ?>
         
         <div class="row">        
             <div class="col-md-8 album">
