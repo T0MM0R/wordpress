644857ec5cb10ac10c662f7b7eb6f0b7efcddf43
twilson <twilson@rbc.org>
2014-12-12 22:01:26 -0500

    trouble shooting horizontal scroll

--- wp-content/themes/my-portfolio-tw/single-work.php
+++ wp-content/themes/my-portfolio-tw/single-work.php
@@ -1,13 +1,11 @@
 <?php get_header();?>
-
-<div class="row">
     
     <div class="container">
 
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        
+
         <div class="card">
-        
+
             <h3><?php the_title(); ?></h3>
 
             <hr>
@@ -17,22 +15,20 @@
                 <?php the_content(); ?>
 
             </article>
-            
+
             <a class="btn btn-primary" href="<?php the_field('project_url'); ?>">View Project &rarr;</a>
-                
+
             <img class="img img-responsive" src="<?php the_field( 'homepage_slider_image' ); ?>">
 
         </div>
 
-            <?php endwhile; else: ?>
+        <?php endwhile; else: ?>
 
-                <p>Page not found</p>
+            <p>Page not found</p>
 
-            <?php endif; ?>
-            
-        </div>
+        <?php endif; ?>
 
-</div>
+    </div>
 
 <?php get_footer();?>
 
