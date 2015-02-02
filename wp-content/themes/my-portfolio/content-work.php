88782e00f3f7ef9a1d5d7fd64c2ba9370a2f1bf9
twilson <twilson@rbc.org>
2015-02-02 14:24:02 -0500

    adjusted portfolio styles

--- wp-content/themes/my-portfolio/content-work.php
+++ wp-content/themes/my-portfolio/content-work.php
@@ -1,13 +1,15 @@
-    <div class="col-md-6 album portfolio">
+    <div class="col-md-6 portfolio">
         
         <a href="<?php the_permalink();?>">
             <img class="img img-responsive" src="<?php the_field( 'homepage_slider_image' ); ?>">
         </a>
         
-        <h4>
-            <a href="<?php the_permalink();?>"><?php the_title(); ?></a>
-        </h4>
-        
-        <?php the_field( 'description' ); ?>
+        <div class="caption">
+            <h4>
+                <a href="<?php the_permalink();?>"><?php the_title(); ?></a>
+            </h4>
+
+            <p><?php the_field( 'description' ); ?></p>
+        </div>
         
     </div>
