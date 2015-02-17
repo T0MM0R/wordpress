5f186904e695e3ac725d5a1c45782420f68465d4
twilson <twilson@rbc.org>
2015-02-17 10:51:38 -0500

    add animate property for header transition

--- wp-content/themes/my-portfolio/js/theme.js
+++ wp-content/themes/my-portfolio/js/theme.js
@@ -54,10 +54,9 @@ jQuery(document).scroll(function(){
     var windowPosition = jQuery(document).scrollTop();
     
     if (windowPosition >= headerChangePosition ) {
-        jQuery("#nav").animate({backgroundColor: "rgba(50, 50, 50, 0.8)"}, 400, 'linear', function(){console.log("test");});
+        jQuery("#nav").animate({opacity: 1}, 400, 'linear', function(){console.log("test");});
         jQuery("#logo h2").hide();
     } else {
-        jQuery("#nav").animate({backgroundColor: "transparent"}, 400, 'linear', function(){console.log("test");});
         jQuery("#logo h2").show();
     }
 });
