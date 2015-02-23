fa87e719042bb1959989f34d9b8fff2cc16817ca
twilson <twilson@rbc.org>
2015-02-23 15:17:06 -0500

    added 404 template to single templates

--- wp-content/themes/my-portfolio/404.php
+++ wp-content/themes/my-portfolio/404.php
@@ -13,16 +13,18 @@
 
 <div>
     <p>We could not find what you were looking for! :(</p>
-    <p>Check out some of our latest work instead</p>
+    
     
     <?php if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-
+    
+        <p>Check out some of our latest work instead</p>
         <?php get_template_part( 'content', 'work' ); ?>
+        <p>Like what you see? Contact us today to book your event!</p>
+        <a href="contact" alt="contact us to book your event!" class="btn btn-primary btn-block">Book your Event &rarr;</a>
 
     <?php endwhile; endif; ?>
     
-    <p>Like what you see? Contact us today to book your event!</p>
-    <a href="contact" alt="contact us to book your event!" class="btn btn-primary btn-block">Book your Event &rarr;</a>
+    
 </div>
 
 <?php get_footer();?>
