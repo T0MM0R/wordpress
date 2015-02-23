b67c869e83d10e08f0f622ed3a67e03784d98951
twilson <twilson@rbc.org>
2015-02-23 15:31:31 -0500

    added 404 template to single templates

--- wp-content/themes/my-portfolio/404.php
+++ wp-content/themes/my-portfolio/404.php
@@ -2,18 +2,10 @@
 <?php get_header();?>
 
 <div>
+    
     <p>We could not find what you were looking for! :(</p>
-    
-    
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    
-        <p>Check out some of our latest blog entries</p>
-        <?php get_template_part( 'content', 'post' ); ?>
-        <p>Looking for a photographer? Contact us today to book your event!</p>
-        <a href="contact" alt="contact us to book your event!" class="btn btn-primary btn-block">Book your Event &rarr;</a>
-
-    <?php endwhile; endif; ?>
-    
+    <p>Looking for a photographer? Contact us today to book your event!</p>
+    <a href="contact" alt="contact us to book your event!" class="btn btn-primary btn-block">Book your Event &rarr;</a>
     
 </div>
 
