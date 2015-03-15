75368926627f0929c7ff339976b2cc4f979b68cd
twilson <twilson@rbc.org>
2015-03-14 22:59:56 -0400

    refactored author info

--- wp-content/themes/my-portfolio-tw/content-post.php
+++ wp-content/themes/my-portfolio-tw/content-post.php
@@ -16,7 +16,6 @@
                     <?php endif; ?>
                 </h3>
                 <ul class="info">
-                    <li>By: <?php the_author(); ?></li>
                     <li>Published in <?php the_category(', '); ?></li>
                     <li>On <?php the_time('F j, Y'); ?></li>
                 </ul>
@@ -44,7 +43,6 @@
                     <?php endif; ?>
                 </h3>
                 <ul class="info">
-                    <li>By: <?php the_author(); ?></li>
                     <li>Published in <?php the_category(', '); ?></li>
                     <li>On <?php the_time('F j, Y'); ?></li>
                 </ul>
@@ -71,7 +69,6 @@
                 <?php endif; ?>
             </h3>
             <ul class="info">
-                <li>By: <?php the_author(); ?></li>
                 <li>Published in <?php the_category(', '); ?></li>
                 <li>On <?php the_time('F j, Y'); ?></li>
             </ul>
@@ -100,7 +97,6 @@
                 <?php endif; ?>
             </h3>
             <ul class="info">
-                <li>By: <?php the_author(); ?></li>
                 <li>Published in <?php the_category(', '); ?></li>
                 <li>On <?php the_time('F j, Y'); ?></li>
             </ul>
@@ -133,7 +129,6 @@
                 <?php endif; ?>
             </h3>
             <ul class="info">
-                <li>By: <?php the_author(); ?></li>
                 <li>Published in <?php the_category(', '); ?></li>
                 <li>On <?php the_time('F j, Y'); ?></li>
             </ul>
@@ -157,21 +152,6 @@
 
 <?php if (is_single()) : ?>
 
-<div class="card clearfix">
-    <h3>About The Author</h3>
-    <div class="album pull-left">
-        <?php echo get_custom_avatar( get_the_author_meta('user_email'), 100 , 'mysteryman' , 'gravatar' ); ?>
-    </div>
-    <div class="col-md-8">
-        <h1><?php the_author(); ?></h1>
-        <ul class="info">
-            <li><a href="mailto:<?php the_author_meta('user_email'); ?>"><i class="fa fa-envelope"></i></a></li>
-            <li><a href="<?php the_author_meta('user_url'); ?>" target="_blank"><i class="fa fa-home"></i></a></li>
-            <li><a href="<?php the_author_meta('twitter'); ?>"><i class="fa fa-twitter fa-2"></i></a></li>
-            <li><a href="<?php the_author_meta('facebook'); ?>"><i class="fa fa-facebook"></i></a></li>
-        </ul>
-        <p><?php the_author_meta('description'); ?></p>
-    </div>
-</div>
+<?php get_template_part( 'content', 'author-single' ); ?>
 
 <?php endif; ?>
