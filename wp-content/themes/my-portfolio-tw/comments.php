3c9b5eb1f434b37e4e56a06309ef032709c224d6
twilson <twilson@rbc.org>
2015-03-14 23:59:25 -0400

    updated content-post

--- wp-content/themes/my-portfolio-tw/comments.php
+++ wp-content/themes/my-portfolio-tw/comments.php
@@ -21,7 +21,7 @@ if ( post_password_required() )
 	return;
 ?>
 
-<div id="comments" class="comments-area">
+<div id="comments" class="comments-area card">
 
 	<?php // You can start editing here -- including this comment! ?>
 
