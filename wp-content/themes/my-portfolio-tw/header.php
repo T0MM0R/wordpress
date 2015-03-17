378c23174c448684cbca2d9df83a333bc0d04073
twilson <twilson@rbc.org>
2015-03-17 10:41:22 -0400

    added logo img to tw

--- wp-content/themes/my-portfolio-tw/header.php
+++ wp-content/themes/my-portfolio-tw/header.php
@@ -31,7 +31,7 @@
 
                 <a href="<?php echo site_url(); ?>">
                     <h1><?php bloginfo( 'name' ); ?> -></h1>
-                    <h2 class="visible-md visible-lg"><?php bloginfo( 'description' ) ;?></h2>
+                    <h2 class="visible-md-inline-block visible-lg-inline-block"><?php bloginfo( 'description' ) ;?></h2>
                 </a>
 
             </hgroup>
