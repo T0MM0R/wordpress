1f08ec4cb2c664387d86182fb4ff18ff13ea0cc7
twilson <twilson@rbc.org>
2015-04-19 17:33:46 -0400

    updated header

--- wp-content/themes/realty/front-page.php
+++ wp-content/themes/realty/front-page.php
@@ -4,7 +4,7 @@
         
         <div class="row">
             
-            <div class="center">
+            <div class="center" style="background: url(<?php echo wp_get_attachment_url(get_post_thumbnail_id()) ?>) center no-repeat; background-attachment: fixed; background-size: cover; height: 600px;">
             
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
