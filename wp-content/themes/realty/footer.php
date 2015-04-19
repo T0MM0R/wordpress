3489bb89073c04de91e6ab37aeba49f6691ade9a
twilson <twilson@rbc.org>
2015-04-19 17:31:38 -0400

    updated footer

--- wp-content/themes/realty/footer.php
+++ wp-content/themes/realty/footer.php
@@ -1,7 +1,6 @@
         <footer>
-            <div id="copyright" class="container-fluid">
-                <p>&copy; Copyright <?php echo date( 'Y' ); ?> Riverside Realty. All Rights Reserved.</p>
-                <p>Site design by <a href="http://thomaswilson.me/" alt="Thomas Wilson Digital Media Consulting">Thomas Wilson Digital Media Consulting</a></p>
+            <div class="container-fluid">
+                <p>&copy; Copyright <?php echo date( 'Y' ); ?> Riverside Realty. All Rights Reserved. Site design by <a href="http://thomaswilson.me/" alt="Thomas Wilson Digital Media Consulting">Thomas Wilson Digital Media Consulting</a></p>
                 <div class="row ss-icon">
                     <a href="facebook">&#xF610;</a>
                     <a href="twitter">&#xF611;</a>
