adb5fba4cd7648d2f9d0d1320a25b011499103d4
twilson <twilson@rbc.org>
2015-04-19 16:07:18 -0400

    added asset folders

---
+++ wp-content/themes/realty/js/theme.js
@@ -0,0 +1,23 @@
+jQuery('.flexslider').flexslider({
+    controlNav: false,
+    directionNav: false
+});
+
+//add html for lightbox
+var $overlay = jQuery('<div id="overlay"></div>');
+var $image = jQuery('<img>');
+$overlay.append($image);
+jQuery('article img').click(function(event){
+
+    event.preventDefault();
+    jQuery("body").append($overlay);
+    $image.attr("src", jQuery(this).parent().attr("href"));
+    $overlay.fadeIn("fast");
+    $overlay.click(function(){
+        $overlay.hide();
+    });
+
+});
+
+
+
