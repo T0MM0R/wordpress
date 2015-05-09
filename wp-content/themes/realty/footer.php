5783d1c4eb9bbe8fdaa588825a69e287fb96f4c9
twilson <twilson@rbc.org>
2015-05-09 18:20:40 -0400

    adjusted homepage styles

--- wp-content/themes/realty/footer.php
+++ wp-content/themes/realty/footer.php
@@ -1,10 +1,12 @@
         <footer>
             <div class="container-fluid">
-                <p>&copy; Copyright <?php echo date( 'Y' ); ?> Riverside Realty. All Rights Reserved. Site design by <a href="http://thomaswilson.me/" alt="Thomas Wilson Digital Media Consulting">Thomas Wilson Digital Media Consulting</a></p>
-                <div class="row ss-icon">
-                    <a href="facebook">&#xF610;</a>
-                    <a href="twitter">&#xF611;</a>
-                    <a href="mailto:riverside.realty@hotmail.com">&#x2709;</a>
+                <div class="row">
+                    <p>&copy; Copyright <?php echo date( 'Y' ); ?> Riverside Realty. All Rights Reserved. Site design by <a href="http://thomaswilson.me/" alt="Thomas Wilson Digital Media Consulting">Thomas Wilson Digital Media Consulting</a></p>
+                    <div class="ss-icon">
+                        <a href="facebook">&#xF610;</a>
+                        <a href="twitter">&#xF611;</a>
+                        <a href="mailto:riverside.realty@hotmail.com">&#x2709;</a>
+                    </div>
                 </div>
             </div>
         </footer>
