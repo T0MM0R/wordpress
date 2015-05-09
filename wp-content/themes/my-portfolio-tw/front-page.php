a98be233f0441cad7dcaed1b76e913fac0496082
twilson <twilson@rbc.org>
2015-05-09 11:57:55 -0400

    adjusted front page styles

--- wp-content/themes/my-portfolio-tw/front-page.php
+++ wp-content/themes/my-portfolio-tw/front-page.php
@@ -2,12 +2,14 @@
 
 
 <div class="container-fluid" style="background: url(<?php echo wp_get_attachment_url(get_post_thumbnail_id()) ?>) center no-repeat; background-attachment: fixed; background-size: cover; height: 600px; text-align: center;">
-    <div class="row">
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <div class="container">
+        <div class="row">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <?php the_content(); ?>
+        <?php the_content(); ?>
 
-    <?php endwhile; endif; ?>
+        <?php endwhile; endif; ?>
+        </div>
     </div>
 </div>
     
