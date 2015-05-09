f764453f0b330f8610d67e8c65c8c52b5df9b3ae
twilson <twilson@rbc.org>
2015-05-09 18:37:10 -0400

    added author to front page

--- wp-content/themes/realty/front-page.php
+++ wp-content/themes/realty/front-page.php
@@ -1,21 +1,40 @@
 <?php get_header();?>
     
-    <div class="container-fluid center" style="background: url(<?php echo wp_get_attachment_url(get_post_thumbnail_id()) ?>) center no-repeat; background-attachment: fixed; background-size: cover; height: 100%;">
+<div class="container-fluid center" style="background: url(<?php echo wp_get_attachment_url(get_post_thumbnail_id()) ?>) center no-repeat; background-attachment: fixed; background-size: cover; height: 100%;">
 
-        <div class="container">
-            
-            <div class="row">
+    <div class="container">
 
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <div class="row">
 
-            <?php the_content(); ?>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-            <?php endwhile; endif; ?>
-                
-            </div>
+        <?php the_content(); ?>
+
+        <?php endwhile; endif; ?>
 
         </div>
-        
+
     </div>
+
+</div>
+
+<div class="container-fluid center">
+    <div class="author">
+        <h3>About The Author</h3>
+
+        <?php echo get_custom_avatar( get_the_author_meta('user_email'), 150 , 'mysteryman' , 'gravatar' ); ?>
+
+
+        <h1><?php the_author(); ?></h1>
+        <ul class="info">
+            <li><a href="mailto:<?php the_author_meta('user_email'); ?>"><i class="fa fa-envelope"></i></a></li>
+            <li><a href="<?php the_author_meta('user_url'); ?>" target="_blank"><i class="fa fa-home"></i></a></li>
+            <li><a href="<?php the_author_meta('twitter'); ?>"><i class="fa fa-twitter fa-2"></i></a></li>
+            <li><a href="<?php the_author_meta('facebook'); ?>"><i class="fa fa-facebook"></i></a></li>
+        </ul>
+        <p><?php the_author_meta('description'); ?></p>
+    </div>
+</div>
+
 
 <?php get_footer();?>
