7e49f293208934a267478687ce0f3ad2872057b3
twilson <twilson@rbc.org>
2015-05-09 22:09:34 -0400

    added new theme for band websites

---
+++ wp-content/themes/band/front-page.php
@@ -0,0 +1,44 @@
+<?php get_header();?>
+    
+<div class="container-fluid center" style="background: url(<?php echo wp_get_attachment_url(get_post_thumbnail_id()) ?>) center no-repeat; background-attachment: fixed; background-size: cover; height: 100%;">
+
+    <div class="container">
+
+        <div class="row">
+
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+        <?php the_content(); ?>
+
+        <?php endwhile; endif; ?>
+
+        </div>
+
+    </div>
+
+</div>
+
+<div class="container">
+    <div class="author">
+        <div class="row">
+            <h1 class="center">Your agent</h1>
+            <div class="avatar center">
+                <?php //echo get_custom_avatar( get_the_author_meta('user_email'), 150 , 'gravatar_default' , 'gravatar' ); ?>
+                <img src="<?php echo get_template_directory_uri() ?>/images/avatar.jpg" class="img-circle" alt="gravatar"/>
+            </div>
+            <div class="bio col-md-6 col-md-offset-3 center">
+                <h1><?php the_author(); ?></h1>
+                <ul class="info">
+                    <li><a href="mailto:<?php the_author_meta('user_email'); ?>"><i class="fa fa-envelope"></i></a></li>
+                    <li><a href="<?php the_author_meta('user_url'); ?>" target="_blank"><i class="fa fa-home"></i></a></li>
+                    <li><a href="<?php the_author_meta('twitter'); ?>"><i class="fa fa-twitter fa-2"></i></a></li>
+                    <li><a href="<?php the_author_meta('facebook'); ?>"><i class="fa fa-facebook"></i></a></li>
+                </ul>
+                <p><?php the_author_meta('description'); ?></p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php get_footer();?>
---
+++ wp-content/themes/band/header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <title>
+            <?php 
+            
+            wp_title( '|', true, 'right' );
+            
+            ?>
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1, user-scalable=0">
+        
+        <?php wp_head(); ?>
+        
+    </head>
+    
+    <body>
+        
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="navbar-header">
+
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="<?php echo site_url(); ?>"><span class="glyphicon glyphicon-home"></span><?php bloginfo( 'name' ); ?></a>
+
+                </div>
+
+                <div class="navbar-collapse collapse" id="nav1">
+
+                   <?php
+                        $args = array(
+                            'menu', 
+                            'main',
+                            'menu_class'      => 'nav navbar-nav',
+                            'walker' => new My_Walker_Nav_Menu()
+                        );
+                        wp_nav_menu( $args );
+                    ?>
+
+                </div>
+            </div>
+        </nav>
+        
