f6e8808a52e890422ec7cd0f580b3c94c4c4d35d
twilson <twilson@rbc.org>
2015-05-18 19:49:46 -0400

    updated band template

--- wp-content/themes/band/footer.php
+++ wp-content/themes/band/footer.php
@@ -1,10 +1,10 @@
     <footer class="container-fluid">
         <div class="row">
-            <p>&copy; Copyright <?php echo date( 'Y' ); ?> Riverside Realty. All Rights Reserved. Site design by <a href="http://thomaswilson.me/" alt="Thomas Wilson Digital Media Consulting">Thomas Wilson Digital Media Consulting</a></p>
+            <p>&copy; Copyright <?php echo date( 'Y' ); ?> JakeDown. All Rights Reserved. Site design by <a href="http://thomaswilson.me/" alt="Thomas Wilson Digital Media Consulting">Thomas Wilson Digital Media Consulting</a></p>
             <div class="ss-icon">
                 <a href="facebook">&#xF610;</a>
                 <a href="twitter">&#xF611;</a>
-                <a href="mailto:riverside.realty@hotmail.com">&#x2709;</a>
+                <a href="mailto:jake@jakedown.com">&#x2709;</a>
             </div>
         </div>
     </footer>
