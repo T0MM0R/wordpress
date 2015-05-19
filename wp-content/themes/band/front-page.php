62e1199bfd2cdb4575604cc904793914f154645d
twilson <twilson@rbc.org>
2015-05-18 23:15:02 -0400

    updated band template

--- wp-content/themes/band/front-page.php
+++ wp-content/themes/band/front-page.php
@@ -1,6 +1,6 @@
 <?php get_header();?>
     
-<div class="container-fluid center" style="background: url(<?php echo wp_get_attachment_url(get_post_thumbnail_id()) ?>) center no-repeat; background-attachment: fixed; background-size: cover; height: 600px;">
+<div class="container-fluid center" style="background: url(<?php echo wp_get_attachment_url(get_post_thumbnail_id()) ?>) 0 -100px no-repeat; background-attachment: fixed; background-size: cover; height: 600px;">
 
     <div class="container">
 
