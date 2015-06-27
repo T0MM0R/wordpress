cfd7809952f44d7b8502e1f6d8f416365e42c78e
twilson <twilson@rbc.org>
2015-06-27 15:34:53 -0400

    updated content-post

--- wp-content/themes/band/content-post.php
+++ wp-content/themes/band/content-post.php
@@ -3,7 +3,7 @@
         <?php if ( has_post_thumbnail() ): ?>
         <?php the_post_thumbnail('thumbnail', array('class' => 'img img-responsive pull-left')) ?>
         <?php endif ?>
-        <h1><a href="<?php the_permalink() ?>" title="<?php the_title() ?>"><?php the_title() ?></a></h1>
+        <h2><a href="<?php the_permalink() ?>" title="<?php the_title() ?>"><?php the_title() ?></a></h2>
         <span>Published on: <?php the_date() ?></span>
         <article><?php the_excerpt() ?></article>
     </div>
