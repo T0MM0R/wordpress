559563d5181f1baf89ac2ed3926bbecdfe4aaa6f
twilson <twilson@rbc.org>
2015-06-27 15:44:07 -0400

    updated templates

---
+++ wp-content/themes/band/content-single.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="row">
+        <div class="single">
+            <h1><?php the_title() ?></h1>
+            <span>Published on: <?php the_date() ?></span>
+            <article><?php the_post() ?></article>
+        </div>
+    </div>
+</div>
+
