48553ecdedaadc228c7ad4855d2109889ae89363
twilson <twilson@rbc.org>
2015-06-27 15:50:40 -0400

    updated templates

--- wp-content/themes/band/content-post.php
+++ wp-content/themes/band/content-post.php
@@ -1,11 +1,13 @@
 <div class="col-md-4">
     <div class="card">
-        <?php if ( has_post_thumbnail() ): ?>
-        <?php the_post_thumbnail('thumbnail', array('class' => 'img img-responsive pull-left')) ?>
-        <?php endif ?>
-        <h2><a href="<?php the_permalink() ?>" title="<?php the_title() ?>"><?php the_title() ?></a></h2>
-        <span>Published on: <?php the_date() ?></span>
-        <section>
+        <section class="headline">
+            <?php if ( has_post_thumbnail() ): ?>
+            <?php the_post_thumbnail('thumbnail', array('class' => 'img img-responsive pull-left')) ?>
+            <?php endif ?>
+            <h2><a href="<?php the_permalink() ?>" title="<?php the_title() ?>"><?php the_title() ?></a></h2>
+            <span>Published on: <?php the_date() ?></span>
+        </section>
+        <section class="body">
             <article><?php the_excerpt() ?></article>
         </section>
     </div>
