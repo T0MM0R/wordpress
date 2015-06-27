4ad09c1670d1b581557174d34fcc785b9b75d2c2
twilson <twilson@rbc.org>
2015-06-27 15:21:54 -0400

    updated content-post

--- wp-content/themes/band/content-post.php
+++ wp-content/themes/band/content-post.php
@@ -1,6 +1,10 @@
 <div class="col-md-4">
     <div class="card">
-        <h1><?php the_title() ?></h1>
+        <?php if ( has_post_thumbnail() ): ?>
+        <?php the_post_thumbnail('thumbnail', array('class' => 'img img-responsive pull-left')) ?>
+        <?php endif ?>
+        <h1><a href="<?php the_permalink ?>" title="<?php the_title() ?>"><?php the_title() ?></a></h1>
+        <h2><?php the_category() ?></h2>
         <span>Published on: <?php the_date() ?></span>
         <article><?php the_excerpt() ?></article>
     </div>
