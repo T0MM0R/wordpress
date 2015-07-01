ea382f205f619aaba31c2864a5762b99a74f940f
twilson <twilson@rbc.org>
2015-07-01 08:33:08 -0400

    updated templates

---
+++ wp-content/themes/band/content-featured.php
@@ -0,0 +1,18 @@
+
+<div class="card-featured">
+    <section class="featured-image col-md-4">
+        <?php if ( has_post_thumbnail() ): ?>
+        <?php the_post_thumbnail('thumbnail', array('class' => 'img img-responsive')) ?>
+        <?php endif ?>
+    </section>
+    <section class="col-md-8">
+        <section class="headline">
+            <h2><a href="<?php the_permalink() ?>" title="<?php the_title() ?>"><?php the_title() ?></a></h2>
+            <span>Published on: <?php the_date() ?></span>
+        </section>
+        <section class="body">
+            <article><?php the_excerpt() ?></article>
+        </section>
+        <a class="btn btn-default btn-block" href="<?php the_permalink() ?>" title="Read more about <?php the_title() ?>">Read More &rarr;</a>
+    </section>
+</div>
--- wp-content/themes/band/home.php
+++ wp-content/themes/band/home.php
@@ -1,6 +1,19 @@
 <?php get_header() ?>
 
 <div id="main" class="container">
+    
+    
+    <?php $featured_post = new WP_Query( array('post_type' => 'post', 'cat' => 3, 'posts_per_page' => 1) ) ?>
+
+    <?php if ( $featured_post->have_posts() ): while ( $featured_post->have_posts() ): $featured_post->the_post() ?>
+    
+    <div class="row">
+        <?php get_template_part('content', 'featured') ?>
+    </div>
+    
+    <?php endwhile; endif; ?>
+    
+    
     <div class="row">
         <?php if ( have_posts() ): while ( have_posts() ): the_post() ?>
         
@@ -8,6 +21,7 @@
         
         <?php endwhile; endif; ?>
     </div>
+    
 </div>
 
 <?php get_footer() ?>
