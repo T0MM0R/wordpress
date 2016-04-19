69f08f66fbe32dc9945861a87df4251262afe545
twilson <twilson@rbc.org>
2016-04-19 15:40:45 -0400

    added clearfix

--- wp-content/themes/my-portfolio/content-work.php
+++ wp-content/themes/my-portfolio/content-work.php
@@ -11,5 +11,9 @@
 
             <p><?php the_field( 'description' ); ?></p>
         </div>
+        
+        <?php if ($wp_query->post_count%2): ?>
+        <div class="clearfix"></div>
+        <?php endif; ?>
 
     </div>
